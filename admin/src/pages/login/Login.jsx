7450c26c9f69ae43fa2a6679955ebf5e64c5f010
Rainbow <kenesou@gmail.com>
2023-04-14 22:38:59 +1200

    add login page and update widgetSm in component

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-//import { login } from "../../redux/apiCalls";
+import { login } from "../../redux/apiCalls";
 
-export const Login = () => {
+const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const dispatch = useDispatch();
 
     const handleClick = (e) => {
         e.preventDefault();
-        //login(dispatch, { username, password });
+        login(dispatch, { username, password });
     };
 
   return (
@@ -40,3 +40,5 @@ export const Login = () => {
     </div>
   );
 };
+
+export default Login;
