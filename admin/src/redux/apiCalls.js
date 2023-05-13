a8bfb16a5ae90ff962ea12f233ee61f53685117c
Rainbow <kenesou@gmail.com>
2023-05-13 13:48:33 +1200

    update the admin board

--- admin/src/redux/apiCalls.js
+++ admin/src/redux/apiCalls.js
@@ -1,5 +1,19 @@
 import { publicRequest } from "../requestMethods";
 import {loginFailure, loginStart, loginSuccess} from "./userRedux";
+import {
+    getProductFailure,
+    getProductStart,
+    getProductSuccess,
+    deleteProductFailure,
+    deleteProductStart,
+    deleteProductSuccess,
+    updateProductFailure,
+    updateProductStart,
+    updateProductSuccess,
+    addProductFailure,
+    addProductStart,
+    addProductSuccess,
+  } from "./productRedux";
 
 export const login = async (dispatch, user) =>{
     dispatch(loginStart());
@@ -9,4 +23,25 @@ export const login = async (dispatch, user) =>{
     }catch(err){
         dispatch(loginFailure());
     }
-}
+};
+
+export const getProducts = async (dispatch) => {
+    dispatch(getProductStart());
+    try {
+        const res = await publicRequest.get("/products");
+        dispatch(getProductSuccess(res.data));
+    } catch (err) {
+        dispatch(getProductFailure());
+    }
+  };
+
+  export const deleteProduct = async (id, dispatch) => {
+    dispatch(deleteProductStart());
+    try {
+        //do not want to delete products right now
+        //const res = await userRequest.get(`/products/${id}`);
+        dispatch(deleteProductSuccess(id));
+    } catch (err) {
+        dispatch(deleteProductFailure());
+    }
+  };
